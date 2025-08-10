6d8d547894ae214877bcc55951ce85612823e343
Andrew Li <andrew.li2277@gmail.com>
2025-08-10 16:52:30 +1000

    Creating adblock extension and udpated focusmode to apply for all websites

--- FocusMode/background.js
+++ FocusMode/background.js
@@ -4,22 +4,16 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-const extensions = 'https://developer.chrome.com/docs/extensions';
-const webstore = 'https://developer.chrome.com/docs/webstore';
-
 chrome.action.onClicked.addListener(async (tab) => {
   // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
   // Next state will always be the opposite
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-  }
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON';    
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
   
   if (nextState === "ON") {
     // Insert the CSS file when the user turns the extension on
